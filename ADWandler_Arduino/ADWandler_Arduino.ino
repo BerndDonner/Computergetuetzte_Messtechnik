712b4a5178a51b6cf5db592a0928ea8bf9f9aadd
BerndDonner <119803636+BerndDonner@users.noreply.github.com>
2024-12-16 14:48:46 +0100

    AD Wandler Praxis

---
+++ ADWandler_Arduino/ADWandler_Arduino.ino
@@ -0,0 +1,13 @@
+
+int analogPin{A5};  // Pin, der gelesen werden soll: Pin A3
+
+int val{0};         // Variable, die den gelesenen Wert speichert
+
+void setup() {
+  Serial.begin(115200);  // Setup der seriellen Verbindung
+}
+
+void loop() {
+  val = analogRead(analogPin);  // Pin einlesen
+  Serial.println(val);          // Wert ausgeben
+}
